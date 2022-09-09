454ff8e52bc25d10b2e675b83df536a20923e1ef
topestar <topestar@live.com>
2022-09-08 21:21:49 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  * Description: main - entry point
+ *   *main: prints the phrase Programming is like building a multilingual puzzle
+ *    *Return: 0 if exited properly, non-zero otherwise
+ *     */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
