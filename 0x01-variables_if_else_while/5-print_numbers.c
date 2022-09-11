0591dc7b13e314c21f415bf6ad4b478a4ee2a877
topestar <topestar@live.com>
2022-09-11 05:40:34 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print digit numbers of base 10 
+ * starting from 0, followed by a new line.
+ * return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		print("%d" , n);
+	printf("\n"0);
+	return (0);
+}
+
